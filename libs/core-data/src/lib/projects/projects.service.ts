4d287e4301ab76de55f889ae7800d123867875e8
Steve Witman <stevewitman@gmail.com>
2020-02-24 13:53:46 -0700

    Added crud functions to projects service

--- libs/core-data/src/lib/projects/projects.service.ts
+++ libs/core-data/src/lib/projects/projects.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from './project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() { }
+  projectsUrl = 'https://server-30-x-30.herokuapp.com/projects';
+
+  constructor(private httpClient: HttpClient) { }
+
+  all(): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(this.projectsUrl)
+  }
+
+  findOne(projectId: string): Observable<Project> {
+    return this.httpClient.get<Project>(this.projectsUrl + '/' + projectId);
+  }
+
+  create(project: Project): Observable<Project> {
+    return this.httpClient.post<Project>(this.projectsUrl, project);
+  }
+
+  update(project: Project): Observable<Project> {
+    return this.httpClient.patch<Project>(this.projectsUrl, project);
+  }
+
+  delete(project: Project): Observable<{}> {
+  return this.httpClient.delete<{}>(this.projectsUrl + '/' + project.id)
+  }
+
 }
